a34b2ee48446bbd36eeb58cbea99f430c1992add
Abhinav Thota <59943893+abeeto@users.noreply.github.com>
2025-02-20 15:33:38 +0530

    Fix minor bug, Node now returns value when asked

--- Node.js
+++ Node.js
@@ -1,10 +1,19 @@
-class Node {
+export default class Node {
   #left;
   #right;
   #value;
   constructor(left, right, value) {
-    this.#value = this.#value;
+    this.#value = value;
     this.#left = left;
     this.#right = right;
+  }
+  get value() {
+    return this.#value;
+  }
+  get left() {
+    return this.#left;
+  }
+  get right() {
+    return this.#right;
   }
 }
